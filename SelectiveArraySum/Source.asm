c9526e406020965469ed55ee01a7b2671299ad98
dhmhgre <dhmhgre@gmail.com>
2020-05-11 19:28:31 +0300

    Step 4 Implerment main body

--- SelectiveArraySum/Source.asm
+++ SelectiveArraySum/Source.asm
@@ -11,13 +11,29 @@ message BYTE "The result is :",0
 ArraySize = LENGTHOF array
 
 .code
+;int SelectiveSummation(int *array, int sample, int *sum){
+;	int index = 0;
+;	while( index < ArraySize ){
+;		if( array[index] <= sample )
+;		{
+;			sum += array[index];
+;		}
+;		index++;
+;	}
+;}
 SelectiveSummation PROC
 
 	ret
 SelectiveSummation ENDP
 
 main PROC
-
+    push OFFSET sum 
+	push sample
+	push OFFSET array
+	call SelectiveSummation  ;int SelectiveSummation(int *array, int sample, int *sum)
+	mov edx, OFFSET message
+	call WriteString
+	call WriteInt
 	exit
 main ENDP
 END main

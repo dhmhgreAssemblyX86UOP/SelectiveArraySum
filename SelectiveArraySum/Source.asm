104fc38016047479cc4e4a6da874137ddb12638f
dhmhgre <dhmhgre@gmail.com>
2020-05-11 19:24:15 +0300

    Step 2 Declare Data

--- SelectiveArraySum/Source.asm
+++ SelectiveArraySum/Source.asm
@@ -4,6 +4,11 @@ INCLUDE Irvine32.inc
 
 
 .data
+array SDWORD 10,60,20,33,72,89,45,65,72,18
+sample SDWORD 50
+sum SDWORD 0
+message BYTE "The result is :",0
+ArraySize = LENGTHOF array
 
 .code
 SelectiveSummation PROC
@@ -12,6 +17,7 @@ SelectiveSummation PROC
 SelectiveSummation ENDP
 
 main PROC
+
 	exit
 main ENDP
 END main
